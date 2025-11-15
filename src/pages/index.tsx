c74462515422db50c269bfaf3e42c398d94b6550
FerryKurniawn <itqan.fk@gmail.com>
2025-11-15 14:43:34 +0700

    feat: add conditional rendering for fetching task

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,17 +9,21 @@ export default function Home() {
     >
       <h1 className="text-6xl font-bold">TodoShyt</h1>
       <p className="text-xl font-light mt-4">Keep your tasks organized</p>
-      <div className=" flex flex-col p-16">
-        {tasks.map((task) => (
-          <div
-            key={task.id}
-            className="flex flex-col gap-4 shadow-md bg-white mt-6 md:w-[1200px] md:min-h-32 md:max-h-[400px] rounded-xl p-6 hover:shadow-lg"
-          >
-            <h1 className="font-bold text-4xl">{task.taskName}</h1>
-            <p className="line-clamp-5 text-lg">{task.description}</p>
-          </div>
-        ))}
-      </div>
+      {tasks && tasks.length > 0 ? (
+        <div className=" flex flex-col p-16">
+          {tasks.map((task) => (
+            <div
+              key={task.id}
+              className="flex flex-col gap-4 shadow-md bg-white mt-6 md:w-[1200px] md:min-h-32 md:max-h-[400px] rounded-xl p-6 hover:shadow-lg"
+            >
+              <h1 className="font-bold text-4xl">{task.taskName}</h1>
+              <p className="line-clamp-5 text-lg">{task.description}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="text-xl font-light">No task nyet.</p>
+      )}
     </div>
   );
 }
