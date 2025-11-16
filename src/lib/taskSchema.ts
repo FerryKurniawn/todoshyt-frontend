1b2ee9f908bb6a8300602e81b909d448cddab512
FerryKurniawn <itqan.fk@gmail.com>
2025-11-16 18:36:45 +0700

    feat: remove taskFormSchema and userId

--- src/lib/taskSchema.ts
+++ src/lib/taskSchema.ts
@@ -2,14 +2,8 @@ import { z } from "zod";
 
 export const taskSchema = z.object({
   id: z.number().optional(),
-  userId: z.string().optional(),
-  taskName: z.string().min(1),
+  taskName: z.string().min(1, "this fields are required"),
   description: z.string(),
 });
 
 export type TaskSchema = z.infer<typeof taskSchema>;
-
-export const taskFormSchema = z.object({
-  taskName: z.string().min(1),
-  description: z.string(),
-});
